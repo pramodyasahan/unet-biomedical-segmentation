98ca78431bc6a91a1309bcc2acb79883aa1ad5f7
Pramodya Sahan Dilushana <sahandil1013@gmail.com>
2024-08-24 17:46:30 +0530

    Fix: Corrected input channel mismatch in DoubleConv class

--- src/unet_parts.py
+++ src/unet_parts.py
@@ -8,7 +8,7 @@ class DoubleConv(nn.Module):
         self.double_conv = nn.Sequential(
             nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
-            nn.Conv2d(in_channels, out_channels, kernel_size=3, padding=1),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, padding=1),
             nn.ReLU(inplace=True),
         )
 
