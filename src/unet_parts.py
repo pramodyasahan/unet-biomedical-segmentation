95b745bae1ec4f54bf0212395964c92910793eb3
Pramodya Sahan Dilushana <sahandil1013@gmail.com>
2024-08-24 17:51:25 +0530

    Fix: Corrected output channel mismatch in UpSample class

--- src/unet_parts.py
+++ src/unet_parts.py
@@ -32,7 +32,7 @@ class DownSample(nn.Module):
 class UpSample(nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
-        self.up = nn.ConvTranspose2d(in_channels, out_channels // 2, kernel_size=2, stride=2)
+        self.up = nn.ConvTranspose2d(in_channels, out_channels, kernel_size=2, stride=2)
         self.conv = DoubleConv(in_channels, out_channels)
 
     def forward(self, x1, x2):
